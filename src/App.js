fc34f3793b387f86e150a04f603b690d06fde20a
polyakog <gpolyakov77@gmail.com>
2022-07-26 17:51:13 +0400

    created a new window - Dialogs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 
 function App() {
@@ -12,7 +13,12 @@ function App() {
 
       <Header />
       <Nav />
-      <Profile />
+      
+      <div className='App-wrapper-content'>
+        <Dialogs />
+        {/* <Profile /> */}
+      </div>
+
 
     </div>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,9 +4,9 @@ import MyPosts from './MyPosts';
 
 
 const Profile = () => {
-    
+
     return (
-        <div className={css.profile}>
+        <div >
             {/* Main pic */}
             <div className={css.main_pic_wrapper}>
                 <img className={css.main_pic} src="https://media.istockphoto.com/vectors/molecules-or-dna-strand-genetic-engineering-neural-network-innovation-vector-id1200528971?k=20&m=1200528971&s=612x612&w=0&h=vOdPI_Rm5cpnIHssulc2Y89U2O0sN3P26Tb49JUVJiI=" alt="" /></div>
@@ -14,16 +14,18 @@ const Profile = () => {
                 <div className={css.avatar}>
                     <img src="https://i.pinimg.com/originals/3e/9c/04/3e9c049cd8ffcbb3f0df3255fc1f47ee.png" alt="" />
                 </div>
+
                 <div className={css.description}>
                     <p className={css.user_name}>Gennadij Polyakov</p>
                     <br />
                     <p>Region: Russia</p>
+                    <p>Unit: ACM</p>
                     <p>Position: Application Engineering</p>
                 </div>
             </div>
 
             <MyPosts />
-            
+
         </div>
     );
 }
