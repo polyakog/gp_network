a5da836efb45cef1a0cdfc94bad4dd65e757bb19
polyakog <gpolyakov77@gmail.com>
2022-07-26 17:52:57 +0400

    added Dialogs

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './Dialogs.module.css'
+
+
+const Dialogs = () => {
+    return (
+        <div className={css.wrapper} >
+            <h2> Dialog window</h2> 
+        </div>
+    )
+}
+
+export default Dialogs;
