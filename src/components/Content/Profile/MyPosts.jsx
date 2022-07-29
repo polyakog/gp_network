bbaea4808b68e6a6767ccf99101ac9736eeffc8b
polyakog <gpolyakov77@gmail.com>
2022-07-29 18:43:53 +0400

    added BLL - array in MyPost.jsx

--- src/components/Content/Profile/MyPosts.jsx
+++ src/components/Content/Profile/MyPosts.jsx
@@ -3,7 +3,21 @@ import css from './MyPosts.module.css';
 import Post from './Post/Post';
 
 
+
+
 const MyPosts = () => {
+
+
+    let postData = [
+        {id: 1, message: 'Very interesting', likeCount: 21, Name: 'Alexey'},
+        {id: 2, message: 'How to add data?', likeCount: 2, Name: 'Anton'},
+        {id: 3, message: 'finally. It is a new good network', likeCount: 51, Name: 'Peter'},
+        {id: 4, message: "I'm agree", likeCount: 41, Name: 'Martin'},
+        {id: 5, message: 'We can share info here', likeCount: 44, Name: 'Alexey'},
+        {id: 6, message: 'I will upload all data', likeCount: 53, Name: 'Gennadij'},
+
+    ]
+
 
     return (
         <div className={css.Posts}>
@@ -18,11 +32,13 @@ const MyPosts = () => {
 
             </div>
 
-            <Post message='Very interesting' likeCount='21' Name='Alexey' />
-            <Post message='How to add data?' likeCount='2' Name='Anton' />
-            <Post message='finally. It is a new good network' likeCount='56' Name='Peter' />
-            <Post message="I'm agree" likeCount='10' Name='Martin' />
-
+            <Post message={postData[0].message} likeCount={postData[0].likeCount} Name={postData[0].Name} />
+            <Post message={postData[1].message} likeCount={postData[1].likeCount} Name={postData[1].Name} />
+            <Post message={postData[2].message} likeCount={postData[2].likeCount} Name={postData[2].Name} />
+            <Post message={postData[3].message} likeCount={postData[3].likeCount} Name={postData[3].Name} />
+            <Post message={postData[4].message} likeCount={postData[4].likeCount} Name={postData[4].Name} />
+            <Post message={postData[5].message} likeCount={postData[5].likeCount} Name={postData[5].Name} />
+                       
 
 
         </div>
