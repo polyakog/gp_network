1848ae46eaac9264a189a008c77f43753515c3c0
polyakog <gpolyakov77@gmail.com>
2022-08-05 20:02:54 +0400

    added function for addButton and reference on the Button

--- src/components/Content/News/News.jsx
+++ src/components/Content/News/News.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import css from "./News.module.css";
 
+
 const News = () => {
 return (
     <div>
         News
+        
+        
     </div>
 
 )
--- src/components/Content/Profile/MyPosts.jsx
+++ src/components/Content/Profile/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import css from './MyPosts.module.css';
 import Post from './Post/Post';
-// import {postData} from './../../../index';
 
 
 
@@ -12,16 +11,26 @@ const MyPosts = (props) => {
  
                 /* Обработка массива postData, чтобы исключить повторения команд. */
                 /* Автоматически данные заносятся в новый цикл "p" из массива "postData" */
-
     let postElements = props.postData.map (p => (<Post message={p.message} likeCount={p.likeCount} Name={p.Name} />) );
+
+                /* Создаем ссылку */
+    let newPostElem = React.createRef ();
+
+                /* Создаем команду для кнопки */
+    let addPost = () => {
+        let text = newPostElem.current.value;
+            alert(text);
+    }
 
     return (
         <div className={css.postsBlock}>
 
             <h3>My posts</h3>
             <div className={css.NewPost}>
-                <textarea className={css.NewPostText}>Write your post hire</textarea>
-                <a className={css.addPostButton}>Add post</a>
+                <textarea className={css.NewPostText} ref={newPostElem} >Write your post here</textarea>
+                                {/* Запускаем функцию addPost при нажатии "onClick" */}
+                <a className={css.addPostButton} onClick={addPost}>Add post</a> 
+                
 
                 {/* Визуализация самих постов */}
             </div>
