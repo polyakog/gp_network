bb1accaa63db03e3ae5437a447a8feedd8defbfb
Gennadij <gpolyakov77@gmail.com>
2022-09-05 15:12:36 +0400

    work with ajax(axios) - post to server

--- src/jquary/index.js
+++ src/jquary/index.js
@@ -1,13 +1,23 @@
 const pageNumberEl = document.querySelector('#img-number');
+const clickMeButton = document.querySelector("#get-images");
+const getTasksButtom = document.querySelector('#show-tasks');
 
-const clickMeButton = document.querySelector("#click-me");
 clickMeButton.addEventListener("click", () => {
     const promis = getImages(pageNumberEl.value);
-    promis.then(onDataRecieved);
+    promis.then(onImageRecieved);
 });
 
+getTasksButtom.addEventListener('click', () => {
+ const promis = getTask ();
+    promis.then(onTaskRecieved);
+});
 
-function onDataRecieved(data) {
+createTask("work").then((data) =>{
+
+console.log(data);
+});
+
+function onImageRecieved(data) {
     data.map(el => {
 
         //   const img  = document.createElement('img');
@@ -19,9 +29,14 @@ function onDataRecieved(data) {
     });
 }
 
+function onTaskRecieved(data) {
+    data.map(el => {
+        const li = document.createElement('li');
+        li.innerHTML = el.title +" ID:"+ el.id;
+        document.querySelector('#tasks-result').appendChild(li);
 
-
-
+    });
+}
 
 
 // 
--- src/jquary/service.js
+++ src/jquary/service.js
@@ -1,9 +1,36 @@
 
 
+// function getImages(pageNumber) {
+
+//     const data = $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+
+//     return data;
+
+// }
+
 function getImages(pageNumber) {
 
-    const data = $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+    const promis = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+    return promis.then((response) => {
+        return response.data;
+    });
 
-    return data;
+}
+
+
+function getTask() {
+
+    const promis = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=4422`);
+    return promis.then((response) => {
+        return response.data;
+    });
+
+}
+
+function createTask(title) {
+    const promis = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=4422&title=${title}`);
+    return promis.then((response) => {
+        return response.data;
+    });
 
 }
