b1385f37a9979e762ab43777a1edd9956f24a7f9
Gennadij <gpolyakov77@gmail.com>
2022-10-25 18:30:46 +0400

    work with redux-forms - Login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,52 @@
 import React from "react";
 import css from '../Login/Login.module.css'
+import { Field, reduxForm } from 'redux-form'
+
 
 const Login = (props) => {
-    return <div className={css.wrapper}>
+    const onSubmit= (formData) => {
+        console.log(formData)
+    }
 
-        <h2>Please login...</h2>
+    return <div className={css.wrapper}>
+        <h2>Login Form</h2>
+        <div>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
     </div>
+   }
+
+const LoginForm =(props) => {
     
+    // const {handleSubmit} = props
+    // return <form onSubmit={props.onSubmit(handleSubmit)}>
+    return <form onSubmit={props.handleSubmit}> 
+        <div>
+            <label >Login:</label>
+            <div>
+              <Field name='login' component='input' type='text' placeholder="your login name"/>  
+            </div>
+        </div>
+        <div>
+            <label >Password:</label>
+            <div>
+                <Field name='password' component='input' type='text' placeholder="your password" />
+            </div>
+            
+        </div>
+        <div>
+            <Field name='rememberMe' component='input' type='checkbox'/>
+            remember me
+        </div>
+        <div>
+            <button className={css.loginButton} >Login</button>
+        </div>
+
+
+    </form>
 }
+
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import sidebarReducer from './sidebar-reducer';
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import ThunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form'
  
 
 
@@ -14,6 +15,7 @@ let reducers = combineReducers({
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
+    form : formReducer,
     
 }); 
 
