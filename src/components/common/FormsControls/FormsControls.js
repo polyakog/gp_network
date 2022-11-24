e3300c68bc5fabcb012fa7ebb0cea420efe495a1
Gennadij <gpolyakov77@gmail.com>
2022-11-24 18:38:29 +0400

    login page refactoring: Field

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,20 +1,22 @@
 import React from "react";
 import css from "./FormsControls.module.css";
 import sign from "../../../assets/images/sign4.jpg"
+import { Field } from "redux-form";
 
 
-const FormControl = ({ input, meta: { touched, error }, ...props }) => {
+const FormControl = ({ input, meta: { touched, error }, label, children, ...props }) => {
     // debugger
     const hasError = touched && error;
     return (
         <div >
-            <label className={css.label}>{props.label}</label>
+            <label className={css.label}>{label}</label>
 
             <div className={(hasError ? css.errorTextarea : " ")}>
-                {props.children}
+                {children}
             </div>
 
-            {touched && error && <div className={css.warningWrap}>
+            {touched && error && 
+            <div className={css.warningWrap}>
                 <img className={css.signPic} src={sign} alt="" />
                 <span className={css.errorSpan}>{error}</span>
             </div>
@@ -35,3 +37,22 @@ export const Input = (props) => {
     const { input, ...restProps } = props;
     return <FormControl {...props}><input className={css.Textarea} {...input} {...restProps} /></FormControl>
 }
+
+
+
+export const createField = (error, name, label, component, type, placeholder, autocomplete, validate, text) => (
+        <div className={error ? css.errorField : ''}>
+            <Field
+                name = {name}
+                label={label}
+                component={component}
+                type={type}
+                placeholder={placeholder}
+                autocomplete={autocomplete}
+                validate={validate}
+            /> {text}
+        </div>
+
+
+
+    )
