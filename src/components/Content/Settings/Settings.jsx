14da18d5651e87072dfa5bad54698b027623671c
Gennadij <gpolyakov77@gmail.com>
2022-12-06 20:02:06 +0400

    refactoring settings: button is not assigned

--- src/components/Content/Settings/Settings.jsx
+++ src/components/Content/Settings/Settings.jsx
@@ -4,8 +4,11 @@ import css from "./Settings.module.css"
 const Settings = () => {
     return (
         <div>
-            Settings
+            <h2>Settings</h2>
+            <p>Global Settings</p> 
+            <button className={css.settingsConnection} onClick = {()=> ( window.alert("your settings is saved"))}>ok</button>
         </div>
+
     )
 }
 
