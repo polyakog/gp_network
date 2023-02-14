c93c20bfc9f4fdf0157676f1a951ae15d4c03b00
Gennadij <gpolyakov77@gmail.com>
2023-02-14 14:16:43 +0400

    Header refactoring: typescript

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,34 @@
 import React from "react";
 import css from './Header.module.css';
 import noPic from '../../assets/images/noPic.jpg'
-import { NavLink, Navigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { DispatchPropsType, MapPropsType } from "./HeaderContainer";
 
 
 
+const Header: React.FC<MapPropsType & DispatchPropsType> = (props) => {
 
-const Header = (props) => {
-
-    // const redirectToLogin =() => {
-    //     <Navigate to='/login' />
-    // }
     return (<header className={css.subheader}>
         {/* <img className={css.subheader_logo} src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Henkel-Logo.svg/640px-Henkel-Logo.svg.png" alt="" /> */}
         <h1 className={css.subheader_name}>API Social Network</h1>
 
         {props.isAuth
             ? (<div className={css.userData}>
-                <div>
+                <div className={css.userLogBut}>
                     <img className={css.userPhoto} src={!props.userPhoto ? noPic : props.userPhoto} alt="avatar" />
                     <button onClick={props.logout}>Log out</button>
                 </div>
 
                 <div className={css.userName}>
-                    <span>userName: {props.login} /</span>
-                    <span>  ID: {props.userId} </span>
+                    <span>Login: {props.login} / ID: {props.userId} </span>
                 </div>
             </div>)
-            : <NavLink to={'/login/'} className={css.login}>LOGIN</NavLink>
+            : (<div>
+                <NavLink to={'/login/'} className={css.login}>LOGIN</NavLink>
+                <div className={css.userName}>
+                    <span>Please login </span>
+                </div>
+                </div>)
             // : <button onClick={redirectToLogin}>LOGIN</button>
         }
 
--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -2,13 +2,11 @@ import React from "react";
 // import { connect } from 'react-redux/es/exports';
 import { connect } from 'react-redux';
 import Header from "./Header";
-import { logout } from './../../redux/auth-reducer.ts';
+import { logout} from './../../redux/auth-reducer';
 import css from './Header.module.css';
+import { AppStateType } from "../../redux/redux-store";
 
-
-
-
-class HeaderContainer extends React.Component {
+class HeaderContainer extends React.Component<MapPropsType & DispatchPropsType> {
 
     // componentDidMount() {
 
@@ -16,31 +14,32 @@ class HeaderContainer extends React.Component {
 
 
     // }
-
-
     render() {
 
         return (<div className={css.header}>
-
-            
-            <Header {...this.props} /> 
-           
+            <Header {...this.props} />
         </div>
-
         )
     }
 }
 
+export type MapPropsType = {
+    login: string|null
+    isAuth: boolean
+    userId: number|null
+    userPhoto: string|null
+}
 
+export type DispatchPropsType = {
+    logout: () => void
+}
 
-let mapStateToProps = (state) => ({
+let mapStateToProps = (state: AppStateType): MapPropsType => ({
     login: state.auth.login,
     isAuth: state.auth.isAuth,
     userId: state.auth.userId,
     userPhoto: state.auth.userPhoto
-
 });
 
-
-export default connect(mapStateToProps, { logout })(HeaderContainer);
+export default connect<MapPropsType,DispatchPropsType,{},AppStateType>(mapStateToProps, { logout })(HeaderContainer);
 
