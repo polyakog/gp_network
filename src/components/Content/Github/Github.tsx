fe53b8deaeaee13c160db06beaec91e169046f61
Gennadij <gpolyakov77@gmail.com>
2023-04-26 10:47:53 +0400

    Github component refactoring: added paginator, timer, settings

--- src/components/Content/Github/Github.tsx
+++ src/components/Content/Github/Github.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import css from './Github.module.css'
 import axios from 'axios'
+import Paginator from '../../common/Paginator/Paginator'
+
 
 type SearchUserDataType = {
     login: string
@@ -9,6 +11,7 @@ type SearchUserDataType = {
 
 type SearchResultType = {
     items: SearchUserDataType[]
+    total_count: number
 }
 
 type UserType = {
@@ -34,7 +37,7 @@ export const Search = (props: SearchPropsType) => {
     }, [props.value])
 
     return (
-        <div>
+        <div className={css.searchWrapper}>
             <input placeholder='search'
                 value={tepmSearch}
                 onChange={(e) => { setTempSearch(e.currentTarget.value) }} />
@@ -51,8 +54,12 @@ export const Search = (props: SearchPropsType) => {
 type UsersListPropType = {
     term: string
     selectedUser: SearchUserDataType | null
-    onUserSelected: (user:SearchUserDataType)=>void
-    
+    onUserSelected: (user: SearchUserDataType) => void
+    per_page: number
+    page: number
+    usersCount: (count: number) => void
+
+
 }
 export const UsersList = (props: UsersListPropType) => {
     const [users, setUsers] = useState<SearchUserDataType[]>([])
@@ -60,35 +67,150 @@ export const UsersList = (props: UsersListPropType) => {
     useEffect(() => {
         console.log('SYNC USERS', props.term)
         axios
-            .get<SearchResultType>(`https://api.github.com/search/users?q=${props.term}`)
+            .get<SearchResultType>(`https://api.github.com/search/users?q=${props.term}&per_page=${props.per_page}&page=${props.page}`)
             .then(res => {
                 setUsers(res.data.items)
+                props.usersCount(res.data.total_count)
             })
+    }, [props.term, props.page, props.per_page])
 
-    }, [props.term])
-
-   return (
-       <ul>
-           {users
-               .map(u => <li key={u.id} className={props.selectedUser === u ? css.selected : ''} onClick={() => {
-                   props.onUserSelected(u)
-
-               }}>
-                   {u.login}
-               </li>)
-           }
-       </ul>
-   )
-
+    return (
+        <ul>
+            {users
+                .map((u, index) => <li key={u.id} className={props.selectedUser === u ? css.selected : ''}
+                    onClick={() => {
+                        props.onUserSelected(u)
+                    }}>
+                    {(props.page-1)*props.per_page+index+1}. {u.login}
+                </li>)
+            }
+        </ul>
+    )
 }
 
+type TimerPropsType = {
+    seconds: number
+    onChange: (actualSeconds: number) => void
+    timerKey: string
+}
+export const Timer = (props: TimerPropsType) => {
+    const [seconds, setSeconds] = useState(props.seconds)
+    useEffect(() => {
+        setSeconds(props.seconds)
+    }, [props.seconds])
 
+    useEffect(() => {
+        props.onChange(seconds)
+    }, [seconds])
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            console.log('TICK')
+            setSeconds((prev) => (prev) - 1)
+        }, 1000)
+        return () => { clearInterval(intervalId) }
+    }, [props.timerKey])
+
+    return <div>{seconds}</div>
+}
+
+type UserDetailsPropsType = {
+    user: SearchUserDataType | null
+}
+
+const startTimerSeconds = 10
+
+export const UserDetails = (props: UserDetailsPropsType) => {
+    const [userDetails, setUserDetails] = useState<null | UserType>(null)
+    const [seconds, setSeconds] = useState(startTimerSeconds)
+
+    useEffect(() => {
+        if (seconds < 1) { setUserDetails(null) }
+    }, [seconds])
+
+    useEffect(() => {
+        console.log('SYNC USER DATAILS')
+        if (!!props.user) {
+            axios
+                .get<UserType>(`https://api.github.com/users/${props.user.login}`)
+                .then(res => {
+                    setSeconds(startTimerSeconds)
+                    setUserDetails(res.data)
+
+                })
+        }
+    }, [props.user])
+
+    return (
+
+        <div>
+            {userDetails && <div>
+                <Timer seconds={seconds} onChange={setSeconds} timerKey={userDetails.id.toString()} />
+                <h3>Details</h3>
+
+                <img src={userDetails.avatar_url} alt="avatar" />
+                <ul>
+                    <li style={{ fontSize: '25px' }}>{userDetails?.login}</li>
+                    <li>{userDetails?.bio}</li>
+                    <li> profile: {userDetails?.html_url}</li>
+                    {/* <li> repositories: {userDetails?.repos_url}</li> */}
+                    <li>{userDetails?.followers} followers</li>
+                </ul>
+            </div>
+            }
+        </div>
+    )
+}
+
+type PageNumberSettingsPropsType = {
+    pagesInput: boolean
+    value: number
+    onSubmit: (fixedValue: number) => void
+    settings: (isVisible: boolean) => void
+}
+
+export const PageNumberSetting = (props: PageNumberSettingsPropsType) => {
+    const [tepmNumber, setTempNumber] = useState<number>(5)
+    useEffect(() => {
+        setTempNumber(props.value)
+    }, [props.value])
+
+    return (
+        <div >
+            {props.pagesInput &&
+                <div className={css.settingPageSize}>
+                    <span> Enter number of users per page </span>
+                    <input placeholder='search'
+                        type={'number'}
+                        value={tepmNumber}
+                        onChange={(e) => { setTempNumber(+e.currentTarget.value) }} />
+
+                    <button onClick={() => {
+                        if (tepmNumber !== null) {
+                            props.onSubmit(tepmNumber)
+                        }
+                        props.settings(false)
+                    }}>Apply</button>
+                    <p> (max = 100) </p>
+                </div>
+            }
+
+        </div>
+
+    )
+}
 export const Github = () => {
     const initialValue = 'polyakog'
     const [selectedUser, setSelectedUser] = useState<SearchUserDataType | null>(null)
-
-    const [userDetails, setUserDetails] = useState<null | UserType>(null)
     const [searchTerm, setSearchTerm] = useState(initialValue)
+
+    const [totalUsersCount, setTotalUsersCount] = useState(100)
+    const [pageSize, setPageSize] = useState(5)
+    const [currentPage, setCurrentPage] = useState(2)
+    const [pagesInput, setPageInput] = useState(false)
+    const onPageChanged = (pageNumber: number) => {
+        setCurrentPage(pageNumber)
+    }
 
     useEffect(() => {
         console.log('SYNC TAB TITLE')
@@ -98,57 +220,47 @@ export const Github = () => {
 
     }, [selectedUser])
 
-    useEffect(() => {
-        console.log('SYNC USER DATAILS')
-        if (!!selectedUser) {
-            axios
-                .get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
-                .then(res => {
-                    setUserDetails(res.data)
-                })
-        }
+    const onPageSizeHandling = () => {
+        setPageInput(true)
 
+    }
 
-    }, [selectedUser])
 
 
     return <div className={css.container}>
         <h2> Github</h2>
         <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" alt='' />
         <div className={css.wrapper}>
+            
             <div className={css.sidebar}>
+                <Paginator totalItemsCount={totalUsersCount} pageSize={pageSize} currentPage={currentPage} onPageChanged={onPageChanged} />
+
                 <Search value={searchTerm} onSubmit={(value) => {
                     setSearchTerm(value)
                 }} />
                 <button className={css.resetButton} onClick={() => { setSearchTerm(initialValue) }}>reset</button>
-                <UsersList term={searchTerm} selectedUser={selectedUser} onUserSelected={setSelectedUser} />
+                <button className={css.settingButton} onClick={onPageSizeHandling} style={{ fontWeight: 'bold' }}>...</button>
+                <span>  users per page: {pageSize}</span>
+                <p style={{fontStyle:'italic'}}>  Total number of users found: {totalUsersCount}</p>
+
+                <PageNumberSetting value={pageSize}
+                    pagesInput={pagesInput}
+                    settings={setPageInput}
+                    onSubmit={(value) => { setPageSize(value) }}
+                />
+
+                <UsersList term={searchTerm}
+                    selectedUser={selectedUser}
+                    onUserSelected={setSelectedUser}
+                    per_page={pageSize}
+                    page={currentPage}
+                    usersCount={setTotalUsersCount}
+                />
             </div>
             <div className={css.details}>
-
-
-                {userDetails && <div>
-                    <h3>Details</h3>
-
-                    <img src={userDetails.avatar_url} alt="avatar" />
-                    <ul>
-                        <li style={{ fontSize: '25px' }}>{userDetails?.login}</li>
-                        <li>{userDetails?.bio}</li>
-                        <li> profile: {userDetails?.html_url}</li>
-                        {/* <li> repositories: {userDetails?.repos_url}</li> */}
-                        <li>{userDetails?.followers} followers</li>
-                    </ul>
-                </div>
-                }
-
-
-
+                <UserDetails user={selectedUser} />
             </div>
 
         </div>
-
-
-
-
     </div>
-
 }
